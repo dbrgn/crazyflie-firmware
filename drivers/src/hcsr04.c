08c06d0a32cf43a8378ba1a86c677bd623b2c46b
Danilo Bargen <mail@dbrgn.ch>
2015-08-14 18:53:51 +0200

    Triggering a measurement works now!

--- drivers/src/hcsr04.c
+++ drivers/src/hcsr04.c
@@ -15,34 +15,58 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * ultrasonic.c: Ultrasonic HC-SR04 driver
+ * hcsr04.c: Ultrasonic HC-SR04 driver
  */
+#define DEBUG_MODULE "DRV_HCSR04"
 
 // ST lib includes
 #include "stm32fxxx.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "debug.h"
+#include "ultrasonic.h"
+
+
 static GPIO_InitTypeDef GPIO_InitStructure;
+
 
 void hcsr04Init()
 {
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_25MHz;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 
     // Configure the regulator enable pin
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
+    GPIO_ResetBits(GPIOC, GPIO_Pin_12);
 
     // Configure trigger pin
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_SetBits(GPIOB, GPIO_Pin_8); // trigger high
 
     // Configure echo pin
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;  // Done by module directly
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+    DEBUG_PRINT("Initialized HC-SR04 sensor\n");
+    GPIO_SetBits(GPIOC, GPIO_Pin_12); // regulator high
+}
+
+
+void hcsr04RequestMeasurement()
+{
+    GPIO_ResetBits(GPIOB, GPIO_Pin_8);
+    vTaskDelay(M2T(1));
+    GPIO_SetBits(GPIOB, GPIO_Pin_8);
 }
--- modules/src/ultrasonic.c
+++ modules/src/ultrasonic.c
@@ -15,18 +15,34 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * ultrasonic.c: Ultrasonic HC-SR04 driver
+ * ultrasonic.c: Ultrasonic expansion board
  */
 #define DEBUG_MODULE "EXP_ULTRASONIC"
+#define TRIGGER_DELAY 50
 
 #include "ultrasonic.h"
 
 #include "hcsr04.h"
+
+#include "FreeRTOS.h"
+#include "timers.h"
+
 #include "debug.h"
 
+
+static void ultrasonicTimer(xTimerHandle timer)
+{
+    hcsr04RequestMeasurement();
+}
 
 void ultrasonicInit(void)
 {
     DEBUG_PRINT("Initializing ultrasonic expansion board\n");
     hcsr04Init();
+
+    // Register timer for ultrasonic trigger
+    xTimerHandle timer = xTimerCreate((const signed char *)"ultrasonicTimer",
+                            M2T(TRIGGER_DELAY),
+                            pdTRUE, NULL, ultrasonicTimer);
+    xTimerStart(timer, 100);
 }
